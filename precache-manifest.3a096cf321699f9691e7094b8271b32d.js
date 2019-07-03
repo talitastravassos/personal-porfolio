b70a8602822cfacf2a22afbd0495e361c9d9d6bf
talitast <talitatravassos.tt@gmail.com>
2019-07-03 11:26:18 -0300

    Updates

---
+++ precache-manifest.3a096cf321699f9691e7094b8271b32d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af491334f6447ce42976",
+    "url": "/static/js/main.af491334.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "af491334f6447ce42976",
+    "url": "/static/css/main.a180cc5e.chunk.css"
+  },
+  {
+    "revision": "6f2d9e5e75cab240ffe6cbd113c0aa71",
+    "url": "/index.html"
+  }
+];
